a78e751e1086838f5becd1bc69732b767855c8f7
yepengxj <yepeng_xj@163.com>
2016-11-11 16:03:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports =  {
     // Enable? true or false
     "enable_cas_auth": true,
         // Point to the CAS authentication URL
-        "cas_server_url": "https://server_name/cas",
+        "cas_server_url": "http://10.1.235.245:14125/citic-uac/login",
 
     // Cookies secret
     "cookie_secret": "REPLACE_WITH_A_RANDOM_STRING_PLEASE",
@@ -16,12 +16,12 @@ module.exports =  {
     proxy_settings : [
         {
         // proxied url
-        proxy_url: "http://127.0.0.1:8023",
+        proxy_url: "http://10.1.235.245:14131",
 
         replaceHostname: false,
 
         // Which port listen to
-        "listen_port": 9500,
+        "listen_port": 80,
         // Enable SSL protocol
         "enable_ssl_port": false,
             // The following settings are valid only when enable_ssl_port is true
@@ -30,11 +30,6 @@ module.exports =  {
             "ssl_key_file": "POINT_TO_YOUR_SSL_KEY",
             // Use absolute path for the certification file
             "ssl_cert_file": "POINT_TO_YOUR_SSL_CERT",
-        },
-        {
-        proxy_url: "http://127.0.0.1:8024",
-        "listen_port": 9501,
-        "enable_ssl_port": false,
         }
     ],
 
